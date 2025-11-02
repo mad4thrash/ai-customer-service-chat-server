a06c7c5abb9453082210e96d3b4506e59df3e3e3
mad4thrash <fabio.plumeri@oisluxurygroup.com>
2025-11-02 19:19:14 +0100

    initial commit

---
+++ index.ts
@@ -0,0 +1,12 @@
+import "dotenv/config";
+import express, { Express, Request, Response } from "express";
+import { MongoClient } from "mongodb";
+import cors from "cors";
+
+const app: Express = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+const client = new MongoClient(process.env.MONGO_DB_ATLAS_URI as string);
